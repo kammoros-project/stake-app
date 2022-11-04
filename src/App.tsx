e210dee9ee146560b40e096941dd0acc9a92e664
Bob <heycryptobob@gmail.com>
2022-11-04 10:35:05 +0800

    tab refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,31 @@ import NotificationCenter from "./components/support/NotificationCenter";
 import NotificationProvider from "./providers/NotificationProvider";
 import { useAddress } from "@thirdweb-dev/react";
 import NFTWrapper from "./components/nft/NFTWrapper";
+import TokenStakingTable from "./components/Token/TokenStakingTable";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
+}
+
+interface IAppTab {
+  text: string
+}
+
+function AppTab({ text }: IAppTab) {
+  return (
+    <Tab
+      className={({ selected }) =>
+        classNames(
+          'w-full rounded-lg p-2 text-xs md:text-sm font-medium leading-5 text-slate-700 outline-0',
+          selected
+            ? 'bg-white shadow'
+            : 'text-slate-100 hover:bg-white/[0.12] hover:text-white'
+        )
+      }
+    >
+      <span>{text}</span>
+    </Tab>
+  )
 }
 
 export default function App() {
@@ -25,38 +47,15 @@ export default function App() {
             <NotificationCenter />
             <Tab.Group>
               <Tab.List className="flex justify-center space-x-1 rounded-xl bg-slate-900/20 p-1 w-full mx-auto mb-16">
-                {/* <Tab
-                  className={({ selected }) =>
-                    classNames(
-                      'w-full rounded-lg p-2 text-xs md:text-sm font-medium leading-5 text-slate-700 outline-0',
-                      selected
-                        ? 'bg-white shadow'
-                        : 'text-slate-100 hover:bg-white/[0.12] hover:text-white'
-                    )
-                  }
-                >
-                  <span>KMC Token</span>
-                </Tab> */}
+                <AppTab text="KMC Token" />
                 {nftStakingContracts.map((tab, key) => (
-                  <Tab
-                    key={key}
-                    className={({ selected }) =>
-                      classNames(
-                        'w-full rounded-lg p-2 text-xs md:text-sm font-medium leading-5 text-slate-700 outline-0',
-                        selected
-                          ? 'bg-white shadow'
-                          : 'text-slate-100 hover:bg-white/[0.12] hover:text-white'
-                      )
-                    }
-                  >
-                    {tab.name}
-                  </Tab>
+                  <AppTab text={tab.name} key={key} />
                 ))}
               </Tab.List>
               <Tab.Panels className="mt-2">
-                {/* <Tab.Panel>
+                <Tab.Panel>
                   <TokenStakingTable />
-                </Tab.Panel> */}
+                </Tab.Panel>
                 {address && nftStakingContracts.map((contract, key) => (
                   <Tab.Panel key={key} >
                     <NFTWrapper contractAddress={contract.address} />
